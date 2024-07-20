a30c6cab9059ca9dfefa5ce5ca6e7cbb6adef9b9
Anjila <gurunganjila27@gmail.com>
2024-07-20 12:48:36 +0545

    updated readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
+import Footer from "./layouts/Footer";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
-      Hello world
+      <div className="container vh-90">
+        <div
+          className="d-flex justify-content-center"
+          style={{ minHeight: "490px" }}
+        >
+          <Home />
+        </div>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
